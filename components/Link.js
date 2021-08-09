ebbc5c1503dea9e8de39286c816f854c97e62d9c
Sahinur Islam <infosahinur@gmail.com>
2021-08-10 03:49:26 +0600

    coustom css fix

--- components/Link.js
+++ components/Link.js
@@ -1,7 +1,7 @@
 function Link({name, image, link}) {
     return (
         <a
-            className='flex items-center w-full max-w-md cursor-pointer p-3 my-3 mx-auto rounded-md r-2 ring-[2px] ring-indigo-400 text-gray-700 backdrop-filter backdrop-blur-2xl bg-white transition duration-200 ease-in-out hover:text-white hover:bg-indigo-300 transform hover:-translate-x-1 hover:scale-90 hover:shadow-xl active:scale-90 '
+            className='flex items-center w-full max-w-md cursor-pointer p-3 my-3 mx-auto rounded-md scale-95 r-2 ring-[2px] ring-indigo-400 text-gray-700 backdrop-filter backdrop-blur-2xl bg-white transition duration-200 ease-in-out hover:text-white hover:bg-indigo-300 transform hover:-translate-x-1 hover:scale-90 hover:shadow-xl active:scale-90 '
             target='_blank'
             href={link}
             >
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <img 
           src="https://avatars.githubusercontent.com/u/68515168?v=4" 
           alt="Sahinur Islam" 
-          className='w-[150px] rounded-full mx-auto'
+          className='w-[190px] rounded-full mx-auto'
         />
         <h2 className='my-3 text-3xl font-bold text-center text-indigo-900 md:text-4xl'>
           Sahinur Islam
