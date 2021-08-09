e9254d18621f94c8c2f55ad5abd9708d5280e7ef
Sahinur Islam <infosahinur@gmail.com>
2021-08-10 04:37:45 +0600

    some file delete

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,8 @@
-import SEO from "@bradgarropy/next-seo";
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <><Component {...pageProps} />
-        <SEO
-        title="Connect with Sahinur"
-        description="An app with all my social links"
-        keywords={["Connect", "links", "PWA"]}
-        icon="/sahinur-islam.jpg"
-        facebook={{
-          image:
-            "https://i.ibb.co/BTh9gJ5/sahinur-dev.jpg",
-          url: "https://sahinur.netlify.app/",
-          type: "website",
-        }}
-        twitter={{
-          image:
-            "https://i.ibb.co/BTh9gJ5/sahinur-dev.jpg",
-          site: "https://sahinur.netlify.app/",
-          card: "summary",
-        }}
-      />
-        </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
