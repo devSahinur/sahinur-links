7f73d483d8e085794fc366ea25cf746b452a2767
Sahinur Islam <infosahinur@gmail.com>
2023-11-13 12:37:50 +0600

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const ip = req.ip
+  res.status(200).json({ name: ip })
 }
